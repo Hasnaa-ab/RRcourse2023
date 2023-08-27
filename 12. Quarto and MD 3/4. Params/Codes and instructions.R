930a7ebd8a316a8ee9a6725aabf74254805d7ac3
Hasnaa <hasnaaabdelghani@gmail.com>
2023-08-27 08:31:11 +0200

    updating HW solutions using parameters to render multiple files

--- 12. Quarto and MD 3/4. Params/Codes and instructions.R
+++ 12. Quarto and MD 3/4. Params/Codes and instructions.R
@@ -1,5 +1,6 @@
 # Rendering
 library(quarto)
+library(reticulate)
 
 quarto_render("QMD_class_3_4.qmd", execute_params = list(
   year = 2017,
@@ -10,14 +11,22 @@ quarto_render("QMD_class_3_4.qmd", execute_params = list(
 
 # Name change
 
-reg <- "Asia"
-y <- 2049
-
-quarto_render("QMD_class_3_4.qmd", execute_params = list(
-  year = y,
-  region = reg,
-  printcode = FALSE,
-  data = "file.csv"
-), output_file = paste0("Report-", reg, "-", y, ".html"))
+for (i in 1:length(season_data$no_season)) {
+  
 
 
+dt <- load("../../Data/season_1.RData")
+s = season_data$no_season[1]
+v = season_data$viewers[1]
+d = season_data$description[1]
+ax1 = season_data$no_season
+ax2 = season_data$viewers
+quarto_render("Assignment.qmd", execute_params = list(
+  season = s,
+  viewers = v,
+  desc = d,
+  y = ax2,
+  x = ax1
+), output_file = paste0("Season ", i , " Report", ".html"))
+
+}
